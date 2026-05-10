80cfa43c5a147227799ece027302bd778a2f0d30
shivakrishna-k <shivakrishna-k@users.noreply.github.com>
2026-05-10 11:43:54 +0800

    fix: convert tailwind config to ESM, remove uninstalled lint script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms'
+
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
@@ -18,5 +20,5 @@ export default {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [forms],
 }
